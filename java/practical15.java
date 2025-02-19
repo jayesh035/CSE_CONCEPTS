f925d08272b203d5e56f0c5d5f4d910a77657c68
jayesh035 <jayeshmakadiya24@gmail.com>
2025-02-19 15:12:31 +0530

    java projects

---
+++ java/practical15.java
@@ -0,0 +1,27 @@
+class Student{
+
+
+    private String name;
+
+    private  int rollNumber;
+
+    private int age;
+
+
+    Student(String name,int rollNumber ,int age){
+        this.name=name;
+        this.rollNumber=rollNumber;
+        this.age=age;
+    }
+
+}
+
+
+
+public  class practical14 {
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java/practical18.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
+
+public  class practical19 {
+
+
+    public static void main(String[] args) {
+
+
+        List<Integer> list= Arrays.asList(1,2,4,5,6,7,100,9);
+
+
+    int sum= list.stream().filter(integer -> integer % 2 ==0).reduce(0,(a,b)->{return a+b; });
+
+        System.out.println(sum);
+    }
+}
